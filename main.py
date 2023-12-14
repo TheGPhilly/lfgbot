16e8bc3f298690ed4d57d520526dfe8d072c18c4
TheGPhilly <135763444+TheGPhilly@users.noreply.github.com>
2023-12-14 16:54:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.ui import View, Button
 from config import TOKEN
 
 intents = discord.Intents.all()
@@ -13,28 +14,12 @@ async def on_ready():
         type=discord.ActivityType.watching, name="♡"))
     await bot.tree.sync()
 
-class SimpleView(discord.ui.View):
-    def __init__(self, ctx):
-        super().__init__()  # times out after 30 seconds
-        # Get the member's voice channel
-        voice_channel = ctx.author.voice.channel
-        # Get the server (guild) ID dynamically
-        server_id = ctx.guild.id
-        button = discord.ui.Button(
-            label='Join',
-            style=discord.ButtonStyle.url,
-            url=f'https://discordapp.com/channels/{server_id}/{voice_channel.id}'
-        )
-        self.add_item(button)
-
-@bot.hybrid_command(name="lfg", description="look for some players.")
-async def lfg(ctx, game: str, message: str):
+@bot.hybrid_command(name="lfg", description="Look for some players.")
+async def lfg(ctx, game: str, *, message: str):
     # Check if the user is in a voice channel
     if not ctx.author.voice:
         await ctx.send(f"{ctx.author.mention}, you are not in a voice channel.", ephemeral=True)
         return
-
-    view = SimpleView(ctx)
     
     # Get the member's avatar URL
     author_avatar_url = ctx.author.avatar.url if ctx.author.avatar else ctx.author.default_avatar.url
@@ -44,20 +29,25 @@ async def lfg(ctx, game: str, message: str):
     
     # Create an embed with the avatar icon in the title and the server avatar
     embed = discord.Embed(
-        title=f"{ctx.author.name} is looking for group",
+        title=f"{ctx.author.name} is looking for a group",
         description=f"{ctx.author.mention} {message}",
         color=discord.Color.blue()
     )
     embed.set_thumbnail(url=guild_icon_url)  # Set author's avatar in the title
-    
     embed.add_field(name="Game", value=game)
-    embed.add_field(name="Voice Channel", value=f"<#{ctx.author.voice.channel.id}>")
     
-    # Send the embed with the view
-    message = await ctx.send(embed=embed, view=view)
-    view.message = message
-
-    await view.wait()
+    # Get the user's current voice channel
+    voice_channel = ctx.author.voice.channel
+    
+    # Create an invite link to the voice channel
+    invite = await voice_channel.create_invite()
+    invite_url = invite.url
+    
+    # Update the embed with the invite link
+    embed.add_field(name="Voice Channel Invite", value=f"[Join]({invite_url})", inline=False)
+    
+    # Send the updated embed
+    await ctx.send(embed=embed)
 
 # Bot token (replace with your own token)
 bot.run(TOKEN)
